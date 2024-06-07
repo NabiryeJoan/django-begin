1af76978875644b70ff1765a660ac190902d02bc
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2024-06-07 16:59:24 +0300

    ✨

--- first/home/url.py
+++ first/home/url.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('home', views.home, name='home')
     path('admin/', admin.site.urls),
      path('home/', include('home.urls'))
 ]
